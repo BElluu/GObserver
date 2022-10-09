2f8e888991dbf1b796923a52fb640955a409dc05
Bartłomiej Komendarczuk <bkomendarczuk@gmail.com>
2022-10-09 22:32:39 +0200

    Implemented SSH connection to server

--- cmd/connect.go
+++ cmd/connect.go
@@ -1,9 +1,17 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"gobserver/data"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
 func init() {
@@ -14,7 +22,72 @@ var connectCmd = &cobra.Command{
 	Use:   "connect",
 	Short: "Connect to server by SSH",
 	Long:  "Connect to server by SSH. Your public key must be on server.",
+	Args:  cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Connecting by SSH...")
+		err := sshConnection(args[0], args[1], args[2])
+		if err != nil {
+			log.Fatal("Something went wrong...")
+		}
 	},
+}
+
+func sshConnection(user, server, port string) error {
+	srv := &data.ServerDetails{Id: server, IpAddress: server, Name: server}
+	file, _ := ioutil.ReadFile("/home/bartek/Programming/GObserver/data/servers.json")
+
+	var xs []map[string]interface{}
+	err := json.Unmarshal(file, &xs)
+	if err != nil {
+		return err
+	}
+	for _, field := range xs {
+		if field["Name"] == srv.Name || field["IpAddress"] == srv.IpAddress || field["Id"] == srv.Id {
+			ip := field["IpAddress"]
+			if port == "" {
+				port = "22"
+			}
+
+			var host = fmt.Sprintf("%s:%s", ip, port)
+
+			fmt.Println("Enter password: ")
+			password, err := terminal.ReadPassword(0)
+
+			sshConfig := &ssh.ClientConfig{
+				User: user,
+				Auth: []ssh.AuthMethod{ssh.Password(string(password))},
+			}
+			sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+
+			client, err := ssh.Dial("tcp", host, sshConfig)
+			if err != nil {
+				return err
+			}
+			input := bufio.NewReader(os.Stdin)
+			fmt.Println("Connected to: " + server)
+			for {
+				session, err := client.NewSession()
+				if err != nil {
+					return err
+				}
+				session.Stdout = os.Stdout
+				command, _ := input.ReadString('\n')
+				command = strings.TrimRight(command, "\r\n")
+
+				if command == "exit" {
+					err := session.Close()
+					if err != nil {
+						return err
+					}
+					break
+				} else {
+					err := session.Run(command)
+					if err != nil {
+						return err
+					}
+				}
+			}
+			return nil
+		}
+	}
+	return nil
 }
